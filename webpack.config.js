715876d2460a9fbc61312a9a8659e07201f68de5
Ryan <bartolay.ryan@gmail.com>
2017-03-24 14:47:34 +0800

    css on webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,11 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.bundle.js'
     },
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+        ]
+    },
     plugins: [new HtmlWebpackPlugin({
         title: 'Project Webpack 2',
         minify: {
